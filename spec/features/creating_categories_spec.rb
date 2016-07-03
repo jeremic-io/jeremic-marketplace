8b9dfcbfef27fe91f53a53c30a212447d80a7468
Dragan Jeremic <draganjeremic01@gmail.com>
2016-07-03 12:22:02 +0200

    Add titles to all pages

--- spec/features/creating_categories_spec.rb
+++ spec/features/creating_categories_spec.rb
@@ -12,6 +12,11 @@ RSpec.feature "Users can create categories" do
     click_button "Create Category"
 
     expect(page).to have_content "Category has been created."
+
+    category = Category.find_by(title: "Cars")
+    expect(page.current_url).to eq category_url(category)
+    title = "Cars - Categories - Jeremic Marketplace"
+    expect(page).to have_title title
   end
 
 end
