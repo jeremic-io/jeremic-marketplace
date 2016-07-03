c26c6c1b872c7bfa738038a340885b5f0edd7f70
Dragan Jeremic <draganjeremic01@gmail.com>
2016-07-03 12:38:24 +0200

    Add validations for title and description in category create. Also test it with r-spec and capybara

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,5 @@
 class Category < ActiveRecord::Base
+  # Validating data #
+  validates :title, presence: true
+  validates :description, presence: true
 end
--- spec/features/creating_categories_spec.rb
+++ spec/features/creating_categories_spec.rb
@@ -2,11 +2,13 @@ require "rails_helper"
 
 RSpec.feature "Users can create categories" do
 
+  before do
+    visit '/categories'
+
+    click_link 'New Category'
+  end
+
   scenario "with valid attributes" do
-    visit "/categories"
-
-    click_link "New Category"
-
     fill_in "Title", with: "Cars"
     fill_in "Description", with: "Simple descriptions"
     click_button "Create Category"
@@ -17,6 +19,15 @@ RSpec.feature "Users can create categories" do
     expect(page.current_url).to eq category_url(category)
     title = "Cars - Categories - Jeremic Marketplace"
     expect(page).to have_title title
+  end
+
+  scenario "when providing invalid attributes" do
+
+    click_button 'Create Category'
+
+    expect(page).to have_content 'Category has not been created.'
+    expect(page).to have_content 'Title can\'t be blank'
+    expect(page).to have_content 'Description can\'t be blank'
   end
 
 end
